023dc497287bed74d1e407dd32b6afcd2e7c29e0
Asia K <66960776+asiakay@users.noreply.github.com>
2025-12-16 00:54:10 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,13 @@
 
+// frontend/vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
